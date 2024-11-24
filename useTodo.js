aa12cc26ac6659fe4865d15f8953eb7dbb9288a6
brayan <brayansamsungfame@gmail.com>
2024-11-24 00:23:52 -0400

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,67 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/TodoReducer";
+
+
+const initialState = [
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false
+    // },
+    
+]
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || []; 
+}
+
+
+export const useTodo = () => {
+
+
+    const [todos, dispatch] = useReducer( todoReducer, initialState, init )
+
+    useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify( todos ) || []);
+    
+    }, [todos])
+
+
+    const handleNewTodo = (todo) => {
+
+        const action = {
+            type: 'Add Todo',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const handleRemoveTodo = (id) => {
+
+        dispatch({
+            type: 'Remove Todo',
+            payload: id
+        });
+
+    }
+
+    const handleToggleTodo = (id) => {
+
+        dispatch({
+            type: 'Toggle Todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        handleNewTodo,
+        handleRemoveTodo,
+        handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter( todo => !todo.done  ).length
+    }
+        
+    
+}
